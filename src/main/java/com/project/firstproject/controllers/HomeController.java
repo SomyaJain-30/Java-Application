7096b3c284345731ee85b39266c001c611a9f909
somyajain30 <somya.jain@devtechnosys.info>
2024-08-26 18:47:09 +0530

    home design

--- src/main/java/com/project/firstproject/controllers/HomeController.java
+++ src/main/java/com/project/firstproject/controllers/HomeController.java
@@ -1,19 +1,30 @@
 package com.project.firstproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "Contact Manager - Home");
         return "home";
     }
     
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        model.addAttribute("title", "Contact Manager - About");
         return "about";
     }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Contact Manager - register");
+        return "register";
+    }
+    
 }
